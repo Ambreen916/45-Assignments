1b4017c44dcbf0a0c50c751ab476bcc19af1d9d1
Ambreen Zubair <159749572+Ambreen916@users.noreply.github.com>
2024-05-21 12:27:14 +0500

    41st task

---
+++ 41st/app.js
@@ -0,0 +1,9 @@
+"use strict";
+// 45 Assignments question # 41
+let magicians = ["Jerry Lewis", "Vincent Price", "Woody Allen", "Edward Norton"];
+function show_magicians(magician) {
+    magician.forEach(magician => {
+        console.log(magician);
+    });
+}
+show_magicians(magicians);
---
+++ 41st/app.ts
@@ -0,0 +1,12 @@
+// 45 Assignments question # 41
+
+let magicians : string [] = ["Jerry Lewis", "Vincent Price", "Woody Allen", "Edward Norton"];
+
+function show_magicians(magician : string[]){
+    magician.forEach(magician => {
+        console.log(magician)
+        
+    });
+}
+
+show_magicians(magicians);
